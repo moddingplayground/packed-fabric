aee42a1565b9e587bfcbfe764b25a7fec18e2833
andantet <orrdaniel73@gmail.com>
2022-04-05 13:42:54 +0100

    authors' backpack

--- src/main/java/net/moddingplayground/packed/impl/client/render/entity/BackpackArmorRendererImpl.java
+++ src/main/java/net/moddingplayground/packed/impl/client/render/entity/BackpackArmorRendererImpl.java
@@ -1,5 +1,6 @@
 package net.moddingplayground.packed.impl.client.render.entity;
 
+import com.google.common.collect.ImmutableList;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -27,10 +28,19 @@ import net.moddingplayground.packed.api.client.model.item.BackpackItemModel;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Environment(EnvType.CLIENT)
 public final class BackpackArmorRendererImpl implements BackpackArmorRenderer, ClientModInitializer {
     public static final Identifier TEXTURE = new Identifier(Packed.MOD_ID, "textures/models/backpack.png");
+    public static final Identifier TEXTURE_AUTHORS = new Identifier(Packed.MOD_ID, "textures/models/backpack_authors.png");
+
+    public static final List<UUID> AUTHORS = ImmutableList.of(
+        UUID.fromString("50c7e7c5-2407-4102-875f-e4b1f49ea61a"), // Andante
+        UUID.fromString("2d173722-de6b-4bb8-b21b-b2843cfe395d")  // Ninni
+    );
+
     private BackpackItemModel<LivingEntity> model;
 
     public BackpackArmorRendererImpl() {}
@@ -49,10 +59,10 @@ public final class BackpackArmorRendererImpl implements BackpackArmorRenderer, C
 
     @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertices, ItemStack stack, LivingEntity entity, EquipmentSlot slot, int light, BipedEntityModel<LivingEntity> model) {
-        this.model.copyFrom(model);
-
-        RenderLayer layer = RenderLayer.getArmorCutoutNoCull(TEXTURE);
+        if (slot != EquipmentSlot.CHEST) return;
+        RenderLayer layer = RenderLayer.getArmorCutoutNoCull(AUTHORS.contains(entity.getUuid()) ? TEXTURE_AUTHORS : TEXTURE);
         VertexConsumer vertex = ItemRenderer.getArmorGlintConsumer(vertices, layer, false, stack.hasGlint());
+        this.model.copyFrom(model);
         this.model.render(matrices, vertex, light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 
